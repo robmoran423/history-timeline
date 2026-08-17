a85688b53dbe66c18d97d9d287d947bff0ac72c3
robertmoran <robmoran423@gmail.com>
2026-08-16 20:31:19 -0400

    initial commit

---
+++ src/main/java/com/example/timeline/Launcher.java
@@ -0,0 +1,8 @@
+package com.example.timeline;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Replace 'MainApp' with the name of your actual JavaFX main class
+        TimelineApp.main(args);
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.moranix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
