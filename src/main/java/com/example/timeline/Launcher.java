2ec59611618597920d94295d3d5be3de8d688a29
robertmoran <robmoran423@gmail.com>
2026-08-17 18:31:53 -0400

    changed to maven launcher

--- src/main/java/com/example/timeline/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.example.timeline;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Replace 'MainApp' with the name of your actual JavaFX main class
-        TimelineApp.main(args);
-    }
-}
